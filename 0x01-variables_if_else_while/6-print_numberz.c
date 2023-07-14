69be3bb4e0e096682560881d8462ec30a9c27722
DJ-MrJay <djmrjay@gmail.com>
2023-07-14 05:54:34 -0700

    Update Print Numberz file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int number;
+
+	number = 0;
+
+	putchar(number + '0');
+
+	while (number < 9)
+	{
+		number++;
+		putchar(number + '0');
+	}
 
 	putchar('\n');
 
