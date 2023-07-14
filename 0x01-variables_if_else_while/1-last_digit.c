913b52081ca28a11796ee696121e49bba439a982
DJ-MrJay <djmrjay@gmail.com>
2023-07-14 04:30:26 -0700

    Update Last Digit script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %d is %d", n, abs(n % 10));
+	printf("Last digit of %d is %d", n, abs(n % 10) * (n < 0 ? -1 : 1));
 
 	if (n % 10 > 5)
 		printf(" and is greater than 5\n");
