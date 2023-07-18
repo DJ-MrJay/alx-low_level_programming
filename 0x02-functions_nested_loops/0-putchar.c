4f75c29606e12875712fc86c211e1e38b32d08e6
DJ-MrJay <djmrjay@gmail.com>
2023-07-18 04:12:43 -0700

    Add main.h and 0-putchar.c files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
+/**
+ * _putchar - Custom function to print a character to stdout.
+ * @c: The character to be printed.
+ *
+ * Return: On success, return the character.
+ * On error, return -1 and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
