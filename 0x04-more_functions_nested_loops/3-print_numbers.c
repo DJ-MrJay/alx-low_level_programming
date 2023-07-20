1b34bd39b4afd7e6f07a73767b49b0237d1a02d0
Jonah Wambua <djmrjay@gmail.com>
2023-07-20 22:18:54 +0300

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,24 +5,24 @@
  * @c: The character to be written.
  *
  * Return: On success, 1. On error,
- * -1 is returned, and errno is set appropriately.
+ * -1 is returned and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return write(1, &c, 1);
 }
 
 /**
- * print_numbers - Prints numbers from 0 to 9 followed by a new line.
+ * print_numbers - Prints numbers from 0 to 9
+ * followed by a new line.
  */
 void print_numbers(void)
 {
-	int num;
+	char num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		_putchar(num + '0');
+		_putchar(num);
 	}
-
 	_putchar('\n');
 }
