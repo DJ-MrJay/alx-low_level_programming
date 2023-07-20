b2139b6ce0b658daccec67b03b14e2271718531e
Jonah Wambua <djmrjay@gmail.com>
2023-07-20 22:57:50 +0300

    Update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,16 +18,16 @@ int _putchar(char c)
  */
 void more_numbers(void)
 {
-    int i, j;
-    
-    for (j = 0; j < 10; j++)
-    {
-        for (i = 0; i <= 14; i++)
-        {
-            if (i > 9)
-                _putchar(i / 10 + '0');
-            _putchar(i % 10 + '0');
-        }
-        _putchar('\n');
-    }
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
