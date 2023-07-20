3f59ed2450e51fe8fd8519410187c7f4d795c109
Jonah Wambua <djmrjay@gmail.com>
2023-07-20 22:07:20 +0300

    Update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,7 @@
  * _putchar - Writes a character to the standard output (stdout).
  * @c: The character to be written.
  *
- * Return: On success, 1. On error, -1 is returned and errno is set appropriately.
+ * Return: On success, 1. On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
@@ -19,7 +19,9 @@ void print_numbers(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
+	{
 		_putchar(num + '0');
+	}
 
 	_putchar('\n');
 }
