b879d619187652a804a25b4afd6d939cd7ce2256
Jonah Wambua <djmrjay@gmail.com>
2023-07-21 01:49:32 +0300

    Fix linter error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - Draws a diagonal line in the terminal using the _putchar function.
+ * print_diagonal - Draws a diagonal line in
+ * the terminal using the _putchar function.
  * @n: The number of times the character '\' should be printed.
  * The diagonal should end with a '\n'.
  * If n is 0 or less, the function should only print a '\n'.
@@ -11,15 +12,19 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');
-	
-	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
