4d48f241e04ede3b62d3a731951a78114cf8371c
Jonah Wambua <djmrjay@gmail.com>
2023-07-21 02:13:37 +0300

    Fix linter errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,14 +6,22 @@
  * 'Fizzbuzz for multiples of both
  */
 int main(void) {
-	for (int i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 == 0) {
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz ");
-		} else if (i % 3 == 0) {
+		}
+		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
-		} else if (i % 5 == 0) {
+		}
+		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
-		} else {
+		}
+		else
+		{
 			printf("%d ", i);
 		}
 	}
