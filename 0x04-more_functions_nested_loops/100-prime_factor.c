eaea2adf1fdebafa933ac14935e7855cc6dd6f0e
Jonah Wambua <djmrjay@gmail.com>
2023-07-21 03:27:56 +0300

    Add Prime Factor file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints largest prime factor of 612852475143
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i, num = 612852475143;
+
+	// Divide by 2 until the number is odd
+	while (num % 2 == 0)
+	{
+		num /= 2;
+	}
+
+	// Now num is odd, start checking odd factors
+	for (i = 3; i <= num; i += 2)
+	{
+		while (num % i == 0 && num != i)
+		{
+			num /= i;
+		}
+	}
+
+	printf("%ld\n", num); // The remaining num is the largest prime factor
+	return (0);
+}
