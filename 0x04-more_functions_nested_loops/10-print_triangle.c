bc5dc27f4216a306f5aa461c2769fd0322c23e60
Jonah Wambua <djmrjay@gmail.com>
2023-07-21 03:06:26 +0300

    Fix linter errors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,32 @@
-# include "main.h"
+#include "main.h"
 
 /**
  * print_triangle - entry point
- * description: i prints diagonals
+ * description: prints a triangle pattern using '#'
  * @size: size of the triangle
  * return: void
 */
-
 void print_triangle(int size)
 {
-    int row, hashes, spaces;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int row, hashes, spaces;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (row = 1; row <= size; row++)
-        {
-            for (spaces = size - row; spaces >= 1; spaces--)
-            {
-                _putchar(' ')
-            }
-            for (hashes = 1; hashes <= row; hashes++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
