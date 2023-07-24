48dc23f2f5c7f2250ecf3fc979f852cb08172c2b
Jonah Wambua <djmrjay@gmail.com>
2023-07-24 13:27:48 +0300

    Update code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,18 +12,15 @@ int len = 0;
 int i;
 char temp;
 
-while (*s != '\0')
+while (s[len] != '\0')
 {
 len++;
-s++;
 }
-
-s--;
 
 for (i = 0; i < len / 2; i++)
 {
-temp = *(s - i);
-*(s - i) = *(s - len + i);
-*(s - len + i) = temp;
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
 }
 }
