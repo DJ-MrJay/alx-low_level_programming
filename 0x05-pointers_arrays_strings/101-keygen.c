190f15637498d0184f255bc715a6218e708a081c
Jonah Wambua <djmrjay@gmail.com>
2023-07-24 14:43:00 +0300

    Update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-    int i;
-    int len = 10;
-    char password[len + 1];
-    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[];:<>,.?/";
+int i;
+int len = 10;
+char password[len + 1];
+char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[];:<>,.?/";
 
-    srand(time(NULL));
+srand(time(NULL));
 
-    for (i = 0; i < len; i++)
-        password[i] = charset[rand() % (sizeof(charset) - 1)];
+for (i = 0; i < len; i++)
+password[i] = charset[rand() % (sizeof(charset) - 1)];
 
-    password[len] = '\0';
+password[len] = '\0';
 
-    printf("%s\n", password);
+printf("%s\n", password);
 
-    return (0);
+return (0);
 }
