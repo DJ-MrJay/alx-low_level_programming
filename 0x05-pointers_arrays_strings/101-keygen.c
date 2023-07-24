8e1fd5888dc016b9d3c25edcaeb0f4ec7127e52f
Jonah Wambua <djmrjay@gmail.com>
2023-07-24 14:41:51 +0300

    Add Keygen file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int len = 10;
+    char password[len + 1];
+    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[];:<>,.?/";
+
+    srand(time(NULL));
+
+    for (i = 0; i < len; i++)
+        password[i] = charset[rand() % (sizeof(charset) - 1)];
+
+    password[len] = '\0';
+
+    printf("%s\n", password);
+
+    return (0);
+}
