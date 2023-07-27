23d63b20dbe118e7dddb3708b70d3ddccf13d8d2
Jonah Wambua <djmrjay@gmail.com>
2023-07-28 02:01:21 +0300

    Update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It declares and initializes an integer variable 'n' without assigning
+ * it any specific value. It also declares an integer array 'a' of size 5,
+ * and a pointer 'p' to an integer. The program then assigns a value of 1024
+ * to the third element of the 'a' array (index 2). Next, it makes the pointer
+ * 'p' point to the address of the integer variable 'n'. However, the program
+ * goes into undefined behavior by attempting to write the value 98 to the
+ * memory location pointed by 'p + 5', which goes beyond the memory allocated
+ * for 'n'. This behavior may result in unpredictable program execution and
+ * potential crashes. Finally, the program prints the value of the third
+ * element of the 'a' array using the 'printf' function.
+ *
+ * Return: Always 0 (indicating success)
+ */
 int main(void)
 {
 int n;
