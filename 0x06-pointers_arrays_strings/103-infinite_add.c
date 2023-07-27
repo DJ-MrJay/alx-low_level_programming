58cf5a5f5e082adefe1ffbb86bf70796e0f6876a
Jonah Wambua <djmrjay@gmail.com>
2023-07-28 02:32:14 +0300

    Update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,7 @@
  * reverse_string - Reverses a string in place
  * @str: The string to reverse
  */
-void reverse_string(char *str)
+static void reverse_string(char *str)
 {
 int start = 0;
 int end = 0;
@@ -25,25 +25,17 @@ end--;
 }
 
 /**
- * infinite_add - Adds two positive numbers and stores the result in a buffer
- * @n1: The first number as a string
- * @n2: The second number as a string
- * @r: The buffer to store the result
- * @size_r: The buffer size
- *
- * Return: Pointer to the result (r) or 0 if the result can't be stored in r.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+* add_numbers - Adds two numbers digit by digit
+* @n1: The first number as a string
+* @n2: The second number as a string
+* @r: The buffer to store the result
+* @size_r: The buffer size
+* @len1: The length of the first number
+* @len2: The length of the second number
+*/
+static void add_numbers(char *n1, char *n2, char *r, int size_r, int len1, int len2)
 {
-int i, j, k, len1, len2, carry = 0;
-
-for (len1 = 0; n1[len1]; len1++)
-;
-for (len2 = 0; n2[len2]; len2++)
-;
-
-if (size_r <= len1 || size_r <= len2)
-return (0);
+int i, j, k, carry = 0;
 
 r[size_r - 1] = '\0';
 len1--;
@@ -70,7 +62,7 @@ len2--;
 if (carry)
 {
 if (k < 0)
-return (0);
+return;
 
 r[k] = carry + '0';
 }
@@ -79,6 +71,30 @@ k++;
 
 if (k >= 0)
 reverse_string(&r[k]);
+}
 
-return (r + k);
+/**
+* infinite_add - Adds two positive numbers and stores the result in a buffer
+* @n1: The first number as a string
+* @n2: The second number as a string
+* @r: The buffer to store the result
+* @size_r: The buffer size
+*
+* Return: Pointer to the result (r) or 0 if the result can't be stored in r.
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int len1 = 0, len2 = 0;
+
+while (n1[len1])
+len1++;
+
+while (n2[len2])
+len2++;
+
+if (size_r <= len1 || size_r <= len2)
+return (0);
+
+add_numbers(n1, n2, r, size_r, len1, len2);
+return (r);
 }
