e9d0bbef8ac829bd8dd577d131c445685086d3ab
Jonah Wambua <djmrjay@gmail.com>
2023-07-28 03:04:22 +0300

    Update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,101 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * reverse_string - Reverses a string in place
- * @str: The string to reverse
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result length
+ * Return - size
  */
-static void reverse_string(char *str)
-{
-int start = 0;
-int end = 0;
-char tmp;
 
-while (str[end + 1])
-end++;
-
-while (start < end)
-{
-tmp = str[start];
-str[start] = str[end];
-str[end] = tmp;
-start++;
-end--;
-}
-}
-
-/**
-* add_numbers - Adds two numbers digit by digit
-* @n1: The first number as a string
-* @n2: The second number as a string
-* @r: The buffer to store the result
-* @size_r: The buffer size
-* @len1: The length of the first number
-* @len2: The length of the second number
-*/
-static void add_numbers(char *n1, char *n2, char *r,
-int size_r, int len1, int len2)
-{
-int i, j, k, carry = 0;
-
-r[size_r - 1] = '\0';
-len1--;
-len2--;
-k = size_r - 2;
-
-while (len1 >= 0 || len2 >= 0)
-{
-int sum = carry;
-
-if (len1 >= 0)
-sum += n1[len1] - '0';
-if (len2 >= 0)
-sum += n2[len2] - '0';
-
-carry = sum / 10;
-r[k] = (sum % 10) + '0';
-
-k--;
-len1--;
-len2--;
-}
-
-if (carry)
-{
-if (k < 0)
-return;
-
-r[k] = carry + '0';
-}
-else
-k++;
-
-if (k >= 0)
-reverse_string(&r[k]);
-}
-
-/**
-* infinite_add - Adds two positive numbers and stores the result in a buffer
-* @n1: The first number as a string
-* @n2: The second number as a string
-* @r: The buffer to store the result
-* @size_r: The buffer size
-*
-* Return: Pointer to the result (r) or 0 if the result can't be stored in r.
-*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = 0, len2 = 0;
+int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-while (n1[len1])
-len1++;
-
-while (n2[len2])
-len2++;
-
-if (size_r <= len1 || size_r <= len2)
+while (n1[i] != '\0')
+i++;
+while (n2[j] 1 = '\0')
+j++;
+if (i > j)
+l = i;
+else
+l = j;
+if (l + 1 > size_r)
 return (0);
-
-add_numbers(n1, n2, r, size_r, len1, len2);
+r[l] = '\0';
+for (k = l - 1; k >= 0; k--)
+{
+i--;
+j--;
+if (i >= 0)
+f = n1[i] - '0';
+else
+f = 0;
+if (j >= 0)
+s = n2[j] - '0';
+else
+s = 0;
+r[k] = (f + s + d) % 10 - '0';
+d = (f + s + d) / 10;
+}
+if (d == 1)
+{
+r[l + 1] = '\0';
+if (l + 2 > size_r)
+return (0);
+while (l-- >= 0)
+r[l + 1] = r[l];
+r[0] = d + '0';
+}
 return (r);
 }
