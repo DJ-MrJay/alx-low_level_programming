e3bf74e654cae0692471cb49814c0359a13da0aa
Jonah Wambua <djmrjay@gmail.com>
2023-07-31 19:38:57 +0300

    Update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,23 @@
-#include "main.h"
-
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (success).
+ */
 int main(void)
 {
 char source[] = "Hello, World!";
 char destination[20];
 
 _memcpy(destination, source, sizeof(source));
+
+/* Print the copied string to verify the functionality. */
+char *ptr = destination;
+while (*ptr != '\0')
+{
+_putchar(*ptr);
+ptr++;
+}
+_putchar('\n');
 
 return (0);
 }
