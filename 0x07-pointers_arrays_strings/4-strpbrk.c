6383a2d08b57ddb5717026f84fc8fb5766a57ae8
Jonah Wambua <djmrjay@gmail.com>
2023-07-31 20:50:02 +0300

    Update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,8 +5,8 @@
  * @s: The string to search.
  * @accept: The string containing accepted bytes.
  *
- * Return: Pointer to the byte in 's' that matches one of the bytes in 'accept',
- *         or NULL if no such byte is found.
+ * Return: Pointer to the byte in 's' that matches one of
+ * the bytes in 'accept', or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
