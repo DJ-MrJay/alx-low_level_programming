4c94ab1e7d98aa436556976e46d295d4dcaf91f7
Jonah Wambua <djmrjay@gmail.com>
2023-08-04 15:06:46 +0300

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,13 +12,13 @@ int wildcmp_helper(char *s1, char *s2);
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
-return 1;
+return (1);
 else if (*s2 == '*')
-return wildcmp_helper(s1, s2 + 1) || (*s1 != '\0' && wildcmp_helper(s1 + 1, s2));
+return (wildcmp_helper(s1, s2 + 1) || (*s1 != '\0' && wildcmp_helper(s1 + 1, s2)));
 else if (*s1 == *s2)
-return wildcmp_helper(s1 + 1, s2 + 1);
+return (wildcmp_helper(s1 + 1, s2 + 1));
 
-return 0;
+return (0);
 }
 
 /**
@@ -31,13 +31,13 @@ return 0;
 int wildcmp_helper(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
-return 1;
+return (1);
 else if (*s1 == *s2)
-return wildcmp_helper(s1 + 1, s2 + 1);
+return (wildcmp_helper(s1 + 1, s2 + 1));
 else if (*s2 == '*')
-return wildcmp(s1, s2);
+return (wildcmp(s1, s2));
 else if (*s1 == '\0')
-return 0;
+return (0);
 
-return 0;
+return (0);
 }
