0274e9a90445bb1064a6d0eaaa3c11a0431e7d34
Jonah Wambua <djmrjay@gmail.com>
2023-08-29 00:23:48 +0300

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t num_nodes = 0;
+size_t num_nodes = 0;
 
-    while (h)
-    {
-        if (!h->str)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 
-        h = h->next;
-        num_nodes++;
-    }
+h = h->next;
+num_nodes++;
+}
 
-    return (num_nodes);
+return (num_nodes);
 }
