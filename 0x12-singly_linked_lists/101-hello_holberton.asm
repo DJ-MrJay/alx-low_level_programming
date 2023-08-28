28c6b7450ca97007c598d524a04b1496c31f1c47
Jonah Wambua <djmrjay@gmail.com>
2023-08-29 00:52:21 +0300

    Update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,11 +3,11 @@ global main       ; Declare the main function as global
 extern printf     ; Declare the printf function
 
 main:
-    mov edi, format  ; Load the address of the format string into edi
-    xor eax, eax     ; Clear eax to indicate no floating point args
-    call printf      ; Call the printf function
-    mov eax, 0       ; Set the return value to 0
-    ret              ; Return from the main function
+mov edi, format  ; Load the address of the format string into edi
+xor eax, eax     ; Clear eax to indicate no floating point args
+call printf      ; Call the printf function
+mov eax, 0       ; Set the return value to 0
+ret              ; Return from the main function
 
 format:
-    db 'Hello, Holberton\n', 0  ; Define the format string and null-terminate it
+db 'Hello, Holberton\n', 0  ; Define the format string and null-terminate it
