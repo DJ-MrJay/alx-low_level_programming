6d5a780708f104c2298747e1dffd1fd86aa518b9
Jonah Wambua <djmrjay@gmail.com>
2023-09-04 20:56:59 +0300

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,38 +12,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer;
-	ssize_t bytes_read, bytes_written, total_written = 0;
+int fd;
+char *buffer;
+ssize_t bytes_read, bytes_written, total_written = 0;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
 
-	while ((bytes_read = read(fd, buffer, letters)) > 0)
-	{
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			free(buffer);
-			close(fd);
-			return (0);
-		}
-		total_written += bytes_written;
-	}
+while ((bytes_read = read(fd, buffer, letters)) > 0)
+{
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+total_written += bytes_written;
+}
 
-	free(buffer);
-	close(fd);
+free(buffer);
+close(fd);
 
-	return (total_written);
+return (total_written);
 }
