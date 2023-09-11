d3974e4fb1229c3a2cf4bc7ce97bbad4023b7f27
Jonah Wambua <djmrjay@gmail.com>
2023-09-12 00:21:34 +0300

    Update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,13 +5,16 @@
  * @n: A pointer to the number to change.
  * @index: The index of the bit to set to 1.
  *
- * Return: 1 for success, -1 for failure.
+ * Return: 1 for success, -1 for failure if index is out of bounds.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
+	int bit_val;
+
 	if (index > 63)
 		return (-1);
 
-	*n = ((1UL << index) | *n);
-	return (1);
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
