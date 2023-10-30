36cc2a8009527c9d7429ba03b5f49d05eaa37165
DJ-MrJay <djmrjay@gmail.com>
2023-10-31 02:28:14 +0300

    Update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -75,14 +75,6 @@ void print_elf_info(const char *filename)
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 
-	print_class(header->e_ident);
-    print_data(header->e_ident);
-    print_version(header->e_ident);
-    print_osabi(header->e_ident);
-    print_abi(header->e_ident);
-    print_type(header->e_type, header->e_ident);
-    print_entry(header->e_entry, header->e_ident);
-
 	free(header);
 	close_elf(o);
 }
